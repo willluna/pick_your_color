bfc3edf6e6495d98edab2f58fbe761359a0e32d3
Will Luna <william.luna@rappi.com>
2024-06-06 09:50:40 -0500

    A new input is added with the value of A to add opacity.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import './App.css';
-import {Box, Button, Typography} from "@mui/material";
+import {Box, Button, Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
 import {useState} from "react";
 import InputBgColor from "./inputBgColor/InputBgColor";
+
 function App() {
   const [r, setR] = useState("");
   const [g, setG] = useState("");
   const [b, setB] = useState("");
+  const [a, setA] = useState("");
   const [bgColor, setBgColor] = useState('');
 
   const handleApply = () => {
-    if (r === "" || g === "" || b === "") {
+    if (r === "" || g === "" || b === "" || a === "") {
       alert("Please enter all values");
-    }else {
-      setBgColor(`rgb(${r}, ${g}, ${b})`);
+    } else {
+      setBgColor(`rgb(${r}, ${g}, ${b}, ${a})`);
     }
   }
 
-  const handleChangeRed = (e) => {
+  const handleChangeR = (e) => {
     let valueBgColor = parseInt(e.target.value);
-    if (isNaN(valueBgColor)) {
-      return;
+    if (valueBgColor === "" || isNaN(valueBgColor)) {
+      setR(valueBgColor);
+      return ;
     } else if (valueBgColor < 0) {
       alert("Please enter a number more than 0");
       valueBgColor = 0;
@@ -30,36 +33,98 @@ function App() {
     setR(valueBgColor);
   }
 
+  const handleChangeG = (e) => {
+    let valueBgColor = parseInt(e.target.value);
+    if (valueBgColor === "" || isNaN(valueBgColor)) {
+      setG(valueBgColor)
+      return;
+    } else if (valueBgColor < 0) {
+      alert("Please enter a number more than 0");
+      valueBgColor = 0;
+    } else if (valueBgColor > 255) {
+      alert("Please enter a number less than 255");
+      valueBgColor = 255;
+    }
+    setG(valueBgColor);
+  }
+  const handleChangeB = (e) => {
+    let valueBgColor = parseInt(e.target.value);
+    if (valueBgColor === "" || isNaN(valueBgColor)) {
+      setB(valueBgColor)
+      return;
+    } else if (valueBgColor < 0) {
+      alert("Please enter a number more than 0");
+      valueBgColor = 0;
+    } else if (valueBgColor > 255) {
+      alert("Please enter a number less than 255");
+      valueBgColor = 255;
+    }
+    setB(valueBgColor);
+  }
+
+  const handleChangeA = (e) => {
+    let valueBgColor = parseInt(e.target.value);
+    if (valueBgColor === "" || isNaN(valueBgColor)) {
+      setA(valueBgColor)
+      return;
+    } else if (valueBgColor < 0) {
+      alert("Please enter a number more than 0");
+      valueBgColor = 0;
+    } else if (valueBgColor > 1) {
+      alert("Please enter a number less than 1");
+      valueBgColor = 1;
+    }
+    setA(valueBgColor);
+  }
 
   return (
-    <Box sx={{display:"flex",  justifyContent:"center", paddingTop:"5rem"}}>
-      <Box
-        sx={{border:"1px solid black", width:"30rem", height:"30rem", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", gap:"1rem"}}
+    <Box sx={{display: "flex", justifyContent: "center", paddingTop: "5rem"}}>
+      <Card
+        sx={{
+          border: "1px solid black",
+          width: "30rem",
+          height: "30rem",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "1rem"
+        }}
       >
-        <Typography variant="h3">...Pick your color</Typography>
-        <Box sx={{width:"80%", height:"70%", border:bgColor, backgroundColor:bgColor}}></Box>
-        <Box sx={{display:"flex", justifyContent:"center", gap:"1rem", padding:"1rem"}}>
-          <InputBgColor
-            placeholder="R"
-            value={r}
-            onChange={handleChangeRed}
-            sx={{border:"1px solid black", width:"100%", height:"100%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", gap:"1rem"}}
-          />
-          <InputBgColor
-            placeholder="G"
-            value={g}
-            onChange={handleChangeValueBgColor(e,setG)}
-          />
-          <InputBgColor
-            placeholder="B"
-            value={b}
-            onChange={handleChangeValueBgColor(e,setB)}
-          />
-          <Button variant="outlined"  onClick={handleApply}>Apply</Button>
-        </Box>
-      </Box>
-    </Box>
-  );
+        <CardContent sx={{ width: "100%", textAlign: "center" }}>
+          <Typography variant="h3">...Pick your color</Typography>
+          <CardMedia
+            component="div"
+            sx={{ width: "100%", height: "10rem", backgroundColor: bgColor, marginTop: "1rem", border: "1px solid black", padding: "1rem" }}
+            />
+        </CardContent>
+            <CardActions>
+              <InputBgColor
+                placeholder="R"
+                value={r}
+                onChange={handleChangeR}
+              />
+              <InputBgColor
+                placeholder="G"
+                value={g}
+                onChange={handleChangeG}
+              />
+              <InputBgColor
+                placeholder="B"
+                value={b}
+                onChange={handleChangeB}
+              />
+              <InputBgColor
+                placeholder="A"
+                value={a}
+                onChange={handleChangeA}
+              />
+              <Button variant="outlined" size="large" onClick={handleApply}>Apply</Button>
+        </CardActions>
+    </Card>
+</Box>
+)
+  ;
 }
 
 export default App;
